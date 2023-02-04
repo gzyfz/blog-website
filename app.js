ff3d87dadcb9533b3ccfaa7cd32d10cc15459292
xiaoyezi <f1ye@ucsd.edu>
2023-02-03 22:39:26 -0800

    v2

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-const _ = require("lodash");
+const mongoose = require('mongoose');
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -16,68 +16,63 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-postArr = [];
+mongoose.connect("mongodb://localhost:27017/blogDB", {useNewUrlParser: true});
 
-app.get("/",function(req,res){
-  res.render("home",{
-      StartingContent:homeStartingContent,
-      postArr:postArr
-    });
-})
+const postSchema = {
+  title: String,
+  content: String
+};
 
-app.get("/about",function(req,res){
-  res.render("about",{
-    AboutContent:aboutContent
+const Post = mongoose.model("Post", postSchema);
+
+app.get("/", function(req, res){
+
+  Post.find({}, function(err, posts){
+    res.render("home", {
+      startingContent: homeStartingContent,
+      posts: posts
+      });
   });
-})
+});
 
-app.get("/contact",function(req,res){
-  res.render("contact",{
-    ContactContent:contactContent
+app.get("/compose", function(req, res){
+  res.render("compose");
+});
+
+app.post("/compose", function(req, res){
+  const post = new Post({
+    title: req.body.postTitle,
+    content: req.body.postBody
   });
-})
 
-app.get("/compose",function(req,res){
-  res.render("compose",{
-  });
-})
 
-app.post("/compose",function(req,res){
-  const POST = {
-    title:req.body.postTitle,
-    content:req.body.postBody
-  }
-  postArr.push(POST);
-  res.redirect("/");
-})
-
-app.get("/posts/:postName",function(req,res){
-  const requestedTitleLower = _.lowerCase(req.params.postName);
-  var elementTitle;
-  var elementContent;
-  postArr.forEach(element => {
-    var elementTitleLower = _.lowerCase(element.title);
-    if(elementTitleLower == requestedTitleLower){
-      elementTitle = element.title;
-      elementContent = element.content;
+  post.save(function(err){
+    if (!err){
+        res.redirect("/");
     }
   });
-  res.render("post",{
-    title:elementTitle,
-    content:elementContent
-  })
-})
+});
 
+app.get("/posts/:postId", function(req, res){
 
+const requestedPostId = req.params.postId;
 
+  Post.findOne({_id: requestedPostId}, function(err, post){
+    res.render("post", {
+      title: post.title,
+      content: post.content
+    });
+  });
 
+});
 
+app.get("/about", function(req, res){
+  res.render("about", {aboutContent: aboutContent});
+});
 
-
-
-
-
-
+app.get("/contact", function(req, res){
+  res.render("contact", {contactContent: contactContent});
+});
 
 
 app.listen(3000, function() {
